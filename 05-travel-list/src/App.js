8e71a850a5cab56773972762cbd869d9e32b5ddd
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-10-11 15:23:53 +0200

    add stats count to footer

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -20,7 +20,7 @@ export default function App() {
       <Logo />
       <Form onAddItems={handleAddItem} />
       <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -101,10 +101,27 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
+
+  const itemsSum = items.length;
+  const packedItems = items.filter((item) => item.packed === true).length;
+  const packedPercent = Math.round((packedItems / itemsSum) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X Items on your list, and you already packed X (X%)</em>
+      {packedPercent === 100 ? (
+        <em>You have packed everything! ✈️</em>
+      ) : (
+        <em>
+          You have {itemsSum} Items on your list, and you already packed {packedItems} ({packedPercent}%)
+        </em>
+      )}
     </footer>
   );
 }
