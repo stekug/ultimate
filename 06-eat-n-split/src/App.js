e510e459f0602eb06c7ad20bf8d90eb41c8e30f6
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-10-24 12:55:07 +0200

    add basic form elements

--- 06-eat-n-split/src/App.js
+++ 06-eat-n-split/src/App.js
@@ -26,7 +26,10 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -60,7 +63,50 @@ function Friend({ friend }) {
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form classname="form-add-friend">
+      <label>🧑‍🤝‍🧑Friend Name</label>
+      <input type="text" />
+
+      <label>🌴Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>🫅Bill Value</label>
+      <input type="text" />
+
+      <label>👨Your expenses</label>
+      <input type="text" />
+
+      <label>🧑‍🤝‍🧑 X´s expenses</label>
+      <input type="text" disabled />
+
+      <label>😊 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split Bill</Button>
+    </form>
   );
 }
